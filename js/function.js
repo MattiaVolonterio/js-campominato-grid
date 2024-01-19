f57a786efbc75f4a8c5f6c292ff76faa8a3b8078
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-19 15:23:24 +0100

    feat: bomb generation

--- js/function.js
+++ js/function.js
@@ -64,3 +64,31 @@ function cellClick(element) {
   element.innerText = element.classList.contains("clicked") ? cellIndex : "";
   console.log("Hai premuto la casella: " + cellIndex);
 }
+
+// ## GENERAZIONE CASUALE DELLE BOMBE IN BASE ALLA DIFFICOLTA'
+
+/**
+ *
+ * @param {HTMLElement} chosenDifficulty l'elemento html corrispondente alla select contenente la difficoltà
+ * @returns un'array contenente 16 numeri casuali compresi nel range di caselle in base alla difficoltà scelta
+ */
+function bombGeneration(chosenDifficulty) {
+  let range;
+  let bombArray = [];
+
+  if (chosenDifficulty.value == "easy") {
+    range = 100;
+  } else if (chosenDifficulty.value == "medium") {
+    range = 81;
+  } else {
+    range = 49;
+  }
+
+  while (bombArray.length < 16) {
+    let randomNumber = Math.floor(Math.random() * (range - 1 + 1) + 1);
+    if (!bombArray.includes(randomNumber)) {
+      bombArray.push(randomNumber);
+    }
+  }
+  return bombArray;
+}
--- js/main.js
+++ js/main.js
@@ -4,12 +4,18 @@ const gridContainer = document.getElementById("grid-container");
 const playButton = document.getElementById("play-button");
 const difficulty = document.getElementById("difficulty-selection");
 
+let bombGeneratedArray = [];
+
 // ## ON LOAD
 
 gridGeneration(gridContainer, difficulty);
+bombGeneratedArray = bombGeneration(difficulty);
+console.table(bombGeneratedArray);
 
 // ## ON PLAY BUTTON CLICK
 
 playButton.addEventListener("click", function () {
   gridGeneration(gridContainer, difficulty);
+  bombGeneratedArray = bombGeneration(difficulty);
+  console.table(bombGeneratedArray);
 });
